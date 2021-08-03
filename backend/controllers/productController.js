f201b01f9de898ca6abfa04040137f30e168511d
ShyamGit01 <shyamasundars43@gmail.com>
2021-08-03 19:21:10 +0530

    product  all endpoint without authentication
    product create, update, delete, show single, show all endpoint done without authentication

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,16 +1,18 @@
 import { Product } from '../models';
 import multer from 'multer';
 import path from 'path';
-import CustomErrorHandler from '../services/customErrorHandler';
-import Joi from 'joi';
+import CustomErrorHandler from '../services/CustomErrorHandler';
 import fs from 'fs';
+import productSchema from '../validator/productValidator';
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => cb(null, 'uploads/'),
     filename: (req, file, cb) => {
-        const uniqueFileName = `${Date.now()}-${Math.round(Math.random()*1E9)}${path.extname(file.originalname)}`;
+        const uniqueFileName = `${Date.now()}-${Math.round(
+            Math.random()*1e9)
+        }${path.extname(file.originalname)}`;
         cb(null, uniqueFileName);
-    } 
+    }, 
 });
 
 const handleMultipartData = multer({storage, limits: { fileSize: 1000000*5}}).single('image');
@@ -24,16 +26,12 @@ const productController = {
 
             const filepath = req.file.path;
 
-            const productSchema = Joi.object({
-                name: Joi.string().required(),
-                price: Joi.number().required(),
-                size: Joi.string().required(),
-            });
-
             const { error } = productSchema.validate(req.body);
             if (error){
                 fs.unlink(`${appRoot}/${filepath}`, (err) => {
-                    return next(CustomErrorHandler.serverError(err.message));
+                    if(err){
+                        return next(CustomErrorHandler.serverError(err.message));
+                    }
                 });
 
                 return next(error);
@@ -54,6 +52,83 @@ const productController = {
 
             res.status(201).json(document);
         })
+    },
+
+    async update(req, res, next) {
+        handleMultipartData(req, res, async (err) => {
+            if (err){
+                return next(CustomErrorHandler.serverError(err.message))
+            }
+
+            let filepath = req.file.path;
+
+            if (req.file){
+                filepath = req.file.path;
+            }
+
+           
+
+            const { error } = productSchema.validate(req.body);
+            if (error){
+            if (req.file){
+                fs.unlink(`${appRoot}/${filepath}`, (err) => {
+                    if(err){
+                        return next(CustomErrorHandler.serverError(err.message));
+                    }
+                });
+            }
+               
+                return next(error);
+            }
+            
+            const { name, price, size } = req.body;
+            let document;
+            try{
+                document = await Product.findOneAndUpdate({_id: req.params.id},{
+                    name,
+                    price,
+                    size,
+                    ...(req.file && {image: filepath})
+                });
+            } catch(err){
+                return next(err);
+            }
+
+            res.status(201).json(document);
+        })
+    },
+
+    async remove(req, res, next) {
+        let documents;
+        try{
+            documents = await Product.findOneAndDelete({_id: req.params.id});
+        } catch(err){
+            return next(CustomErrorHandler.serverError(err.message));
+        }
+        
+        return res.json(documents);
+    },
+    
+    async index(req, res, next) {
+        let documents;
+        try{
+            documents = await Product.find().select('-updatedAt -__v');
+        } catch(err){
+            return next(CustomErrorHandler.serverError(err.message));
+        }
+        
+        return res.json(documents);
+    },
+
+    async show(req, res, next) {
+        let documents;
+        try{
+            documents = await Product.findOne({_id: req.params.id}).select('-updatedAt -__v');
+        } catch(err){
+            return next(CustomErrorHandler.serverError(err.message));
+        }
+        
+        return res.json(documents);
     }
 }
 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,6 +7,11 @@ router.post('/register', registerController.register);
 
 
 router.post('/products', productController.store);
+router.put('/products/:id', productController.update);
+router.delete('/products/:id', productController.remove);
+router.get('/products', productController.index);
+router.get('/products/:id', productController.show);
+
 
 
 
